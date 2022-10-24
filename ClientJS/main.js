aab9406dd4e937106be40f59d4da584a460cfe20
Dontani Fellini <pinkdneal@gmail.com>
2022-10-24 09:33:03 -0400

    cors error fix

--- ClientJS/main.js
+++ ClientJS/main.js
@@ -1,1 +1,6 @@
-alert('working');
+async function apiRequest(){
+    const sharkName = document.querySelector('input'),value
+    try {
+        const response = await fetch (`https://git.heroku.com/don-shark-api.git/api/${sharkName}`)
+    }
+}
